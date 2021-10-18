e0593e016cdb8002ec96a0a57cd13c278beb3a24
Diego Ferigo <diego.ferigo@iit.it>
2021-10-18 15:02:19 +0200

    Install Python resources through pip

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,13 +17,26 @@ cmake ${CMAKE_ARGS} -GNinja .. \
       -DIDYNTREE_USES_OSQPEIGEN:BOOL=ON \
       -DIDYNTREE_USES_IRRLICHT:BOOL=ON \
       -DIDYNTREE_USES_MATLAB:BOOL=OFF \
-      -DIDYNTREE_USES_PYTHON:BOOL=ON \
+      -DIDYNTREE_USES_PYTHON:BOOL=OFF \
       -DIDYNTREE_USES_OCTAVE:BOOL=OFF \
       -DIDYNTREE_USES_LUA:BOOL=OFF \
       -DIDYNTREE_COMPILES_YARP_TOOLS:BOOL=OFF \
-      -DPython3_EXECUTABLE:PATH=$PYTHON \
-      -DIDYNTREE_DETECT_ACTIVE_PYTHON_SITEPACKAGES:BOOL=ON 
+      -DIDYNTREE_DETECT_ACTIVE_PYTHON_SITEPACKAGES:BOOL=ON
 
 cmake --build . --config Release ${NUM_PARALLEL}
 cmake --build . --config Release --target install
 ctest --output-on-failure -C Release
+
+cd ..
+
+python \
+    -m build \
+    --wheel \
+    --outdir dist \
+    --no-isolation \
+    --skip-dependency-check \
+    "-C--global-option=build_ext" \
+    "-C--global-option=-DBUILD_SHARED_LIBS:BOOL=ON" \
+    "-C--global-option=--component=python" \
+    .
+pip install --no-deps dist/*.whl
