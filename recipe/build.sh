f0327f17384f54dd94f320adf4159b41ba732597
Silvio Traversaro <silvio@traversaro.it>
2022-02-09 00:37:18 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,34 +21,3 @@ cmake ${CMAKE_ARGS} -GNinja .. \
 cmake --build . --config Release
 cmake --build . --config Release --target install
 ctest --output-on-failure -C Release
-
-# Fix Python package version
-cd ..
-sed -i.bak "s|use_scm_version=dict(local_scheme=\"dirty-tag\"),|version=\"$PKG_VERSION\",|g" setup.py
-diff -u setup.py{.bak,} || true
-
-# Delete wheel folder
-rm -rf _dist_conda/
-
-# Generate the wheel
-$PYTHON \
-    -m build \
-    --wheel \
-    --outdir _dist_conda \
-    --no-isolation \
-    --skip-dependency-check \
-    "-C--global-option=build_ext" \
-    "-C--global-option=--no-cmake-extension=all" \
-    .
-
-# Install Python package
-pip install \
-  --no-index --find-links=./_dist_conda/ \
-  --no-build-isolation --no-deps \
-  idyntree
-
-# Delete wheel folder
-rm -rf _dist_conda/
-
-# Restore original files
-mv setup.py{.bak,}
