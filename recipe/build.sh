da877867f3bf5a7ebe2cf54a9e02dbe3c1683b3d
Silvio Traversaro <silvio@traversaro.it>
2024-09-04 09:49:53 +0200

    Add --debug-find to debug CMake failures

--- recipe/build.sh
+++ recipe/build.sh
@@ -32,7 +32,8 @@ cmake ${CMAKE_ARGS} -GNinja .. \
       -DIDYNTREE_COMPILES_YARP_TOOLS:BOOL=OFF \
       -DPython3_EXECUTABLE:PATH=$PYTHON \
       -DIDYNTREE_DETECT_ACTIVE_PYTHON_SITEPACKAGES:BOOL=ON \
-      -DIDYNTREE_PYTHON_PIP_METADATA_INSTALLER=conda
+      -DIDYNTREE_PYTHON_PIP_METADATA_INSTALLER=conda \
+      --debug-find
 
 cmake --build . --config Release ${NUM_PARALLEL}
 cmake --build . --config Release --target install ${NUM_PARALLEL}
