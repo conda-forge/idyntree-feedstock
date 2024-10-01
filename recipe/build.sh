57ee6a12313b9e715106b90d4be24f4df012df56
Silvio Traversaro <silvio@traversaro.it>
2024-10-01 10:52:38 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@
 mkdir build
 cd build
 
+Python3_INCLUDE_DIR="$(python -c 'import sysconfig; print(sysconfig.get_path("include"))')"
 Python3_NumPy_INCLUDE_DIR="$(python -c 'import numpy;print(numpy.get_include())')"
 cmake ${CMAKE_ARGS} -GNinja .. \
       -DCMAKE_BUILD_TYPE=Release \
@@ -17,6 +18,7 @@ cmake ${CMAKE_ARGS} -GNinja .. \
       -DIDYNTREE_USES_LUA:BOOL=OFF \
       -DIDYNTREE_COMPILES_YARP_TOOLS:BOOL=OFF \
       -DPython3_EXECUTABLE:PATH=$PYTHON \
+      -DPython3_INCLUDE_DIR:PATH=${Python3_INCLUDE_DIR} \
       -DPython3_NumPy_INCLUDE_DIR=${Python3_NumPy_INCLUDE_DIR} \
       -DIDYNTREE_DETECT_ACTIVE_PYTHON_SITEPACKAGES:BOOL=ON \
       -DIDYNTREE_PYTHON_PIP_METADATA_INSTALLER=conda \
